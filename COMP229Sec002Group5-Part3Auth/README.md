86dfb201320eb14da54329a791cf25133b6f1970
Seunghyeon Ban <munjh564@gmail.com>
2022-12-09 21:01:15 -0500

    part4 backend

---
+++ COMP229Sec002Group5-Part3Auth/README.md
@@ -0,0 +1,14 @@
+# COMP229Sec002Group5
+
+For Centennialcollege COMP229
+
+a Survey site
+(https://comp229sec2group5p3.herokuapp.com/)
+
+By:
+Afsa, Maliha;
+Ban, Seung Hyeon;
+Boglo, Joseph Paul-Daniel;
+Tran, Kevin #301154481;
+Zhou, Yingjie #301179194;
+Wang, Haoyu #301237086;
---
+++ COMP229Sec002Group5-Part3Auth/server/routes/survey.js
@@ -0,0 +1,46 @@
+let express = require('express');
+let router = express.Router();
+let mongoose = require('mongoose');
+
+let jwt = require('jsonwebtoken');
+
+let passport = require('passport');
+
+let surveyController = require('../controllers/survey');
+
+// helper function for guard purposes
+function requireAuth(req, res, next)
+{
+    // check if the user is logged in
+    if(!req.isAuthenticated())
+    {
+        return res.redirect('/login');
+    }
+    next();
+}
+
+/* GET Surveys page. READ*/
+router.get('/', surveyController.displaySurveysPage);
+
+/* GET Create page. CREATE*/
+router.get('/create', requireAuth, surveyController.displayCreatePage);
+
+/* POST proccess Create page. CREATE*/
+router.post('/create', requireAuth, surveyController.processCreatePage);
+
+/* GET Edit page. UPDATE*/
+router.get('/edit/:id', requireAuth, surveyController.displayEditPage);
+
+/* POST process Edit page. UPDATE*/
+router.post('/edit/:id', requireAuth, surveyController.processEditPage);
+
+/* GET process Delete page. DELETE*/
+router.get('/delete/:id', requireAuth, surveyController.processDeletePage);
+
+/* GET Participate page. READ*/
+router.get('/participate/:id', requireAuth, surveyController.displayParticipatePage);
+
+/* Post process Participate page. READ*/
+router.post('/participate/:id', requireAuth, surveyController.processParticapatePage);
+
+module.exports = router;
